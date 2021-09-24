54b6f6c2e207cad54ddd2c82ee3868f4a35f8974
Vitor Risso <vrisso@lifescan.com>
2021-09-24 13:58:41 -0300

    feat: add responsiviness

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AiFillLinkedin, AiFillGithub, AiOutlineMail } from 'react-icons/ai'
 import profilePic from '../assets/profile.jpeg';
 import '../styles/App.css'
 
 function App() {
+  const [iconSize, setIconSize] = useState(45)
+
+  useEffect(() => {
+    if (window.innerWidth <= 400) {
+      setIconSize(30)
+    }
+  }, [])
+  
   return (
     <div className='container'>
       <img src={profilePic} className='profilPicture' alt="Vitor's profile" />
@@ -11,15 +19,15 @@ function App() {
 
       <div className='contactMe'>
         <div className='linkedin contact'>
-          <a href='https://linkedin.com/in/vitor-risso' target='_blank' rel='noreferrer'> <AiFillLinkedin size={45} className='icon' /> LinkedIn</a>
+          <a href='https://linkedin.com/in/vitor-risso' target='_blank' rel='noreferrer'> <AiFillLinkedin size={iconSize} className='icon' /> LinkedIn</a>
         </div>
 
         <div className='github contact'>
-          <a href='https://github.com/vitor-risso' target='_blank' rel='noreferrer'> <AiFillGithub size={45} className='icon' /> Github </a>
+          <a href='https://github.com/vitor-risso' target='_blank' rel='noreferrer'> <AiFillGithub size={iconSize} className='icon' /> Github </a>
         </div>
 
         <div className='maiil contact'>
-          <a href='mailto:contato.vitorrisso@gmail.com' target='_blank' rel='noreferrer'> <AiOutlineMail size={45} className='icon' /> Email </a>
+          <a href='mailto:contato.vitorrisso@gmail.com' target='_blank' rel='noreferrer'> <AiOutlineMail size={iconSize} className='icon' /> Email </a>
         </div>
       </div>
     </div>
